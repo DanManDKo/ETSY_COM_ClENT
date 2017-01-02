3f7b41891783a967b410123bc76bd08368edf45d
USER-PK\User <5699320@gmail.com>
2017-01-02 14:58:52 +0200

    I've begun to build the galleryActivity, but i've got some issue with ProductListActivity. I don't know why it doesn't work =(

--- EtsyClient/app/src/main/java/com/example/user/etsyclient/ui/activity/GalleryActivity.java
+++ EtsyClient/app/src/main/java/com/example/user/etsyclient/ui/activity/GalleryActivity.java
@@ -2,15 +2,44 @@ package com.example.user.etsyclient.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.user.etsyclient.R;
 
 /**
  * Created by User on 01.01.2017.
  */
 
 public class GalleryActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_activity);
+        initViews();
+
+    }
+
+    private void initViews() {
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_gallery);
+        mToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (android.R.id.home):
+                onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
